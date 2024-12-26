d68a0b0c970333603a78fd2be88853eeba220783
NhatTandayy <137869411+Shiengg@users.noreply.github.com>
2024-12-26 08:30:31 +0700

    Update Filter_Button
    Update Fillter Button for Employee Screen

--- lib/screens/stakeholder/employees/employees_screen_state.dart
+++ lib/screens/stakeholder/employees/employees_screen_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:gizmoglobe_client/enums/stakeholders/employee_role.dart';
 import 'package:gizmoglobe_client/objects/employee.dart';
 
 class EmployeesScreenState extends Equatable {
@@ -6,12 +7,14 @@ class EmployeesScreenState extends Equatable {
   final bool isLoading;
   final String searchQuery;
   final int? selectedIndex;
+  final RoleEnum? selectedRoleFilter;
 
   const EmployeesScreenState({
     this.employees = const [],
     this.isLoading = false,
     this.searchQuery = '',
     this.selectedIndex,
+    this.selectedRoleFilter,
   });
 
   EmployeesScreenState copyWith({
@@ -19,15 +22,17 @@ class EmployeesScreenState extends Equatable {
     bool? isLoading,
     String? searchQuery,
     int? selectedIndex,
+    RoleEnum? selectedRoleFilter,
   }) {
     return EmployeesScreenState(
       employees: employees ?? this.employees,
       isLoading: isLoading ?? this.isLoading,
       searchQuery: searchQuery ?? this.searchQuery,
       selectedIndex: selectedIndex,
+      selectedRoleFilter: selectedRoleFilter,
     );
   }
 
   @override
-  List<Object?> get props => [employees, isLoading, searchQuery, selectedIndex];
+  List<Object?> get props => [employees, isLoading, searchQuery, selectedIndex, selectedRoleFilter];
 }
